3232eb5d63f6650943245d4cbe8cacd183f93b54
AhmetKaraduman <karadumana@itu.edu.tr>
2022-04-06 01:29:46 +0300

    change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
 	return (
 		<>
-			<Router basename="/Social-Media-Website">
+			<Router basename="/">
 				<Navbar />
 				<Routes>
 					<Route path="/" element={<TimeFlow />} />
